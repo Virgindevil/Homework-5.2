a47cc6ba8a19698ee43e98aa245785dfa4e3b81c
Сергей Матузов <Ven0m2031@mail.ru>
2022-02-07 16:25:28 +0400

    Добавьте файлы проекта.

---
+++ Homework 5.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Введите предложение:");
+            ReverseWords(Console.ReadLine());
+        }
+
+        public static void ReverseWords(string check)
+        {
+            string[] sentence = TakeWords(check);
+            Console.Write($"\n");
+            Console.WriteLine("Ваше предложение наоборот:");
+            Array.Reverse(sentence);
+            foreach (string word in sentence)
+            {
+                Console.Write($"{word} ");
+            }
+            Console.ReadKey();
+        }
+        public static string[] TakeWords(string words)
+        {
+            string[] sentence = $"{words}".Split(' ', '.', ',', ';');
+            return sentence;
+        }
+    }
+}
